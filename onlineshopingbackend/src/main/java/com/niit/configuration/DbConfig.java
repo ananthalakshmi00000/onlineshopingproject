4a2a2fb8aa1e9a04f5d3c5888e06a314204fa7e3
Charan <Charan@Srinivas>
2019-06-12 11:20:26 +0530

    This is my project creation successfully

---
+++ onlineshopingbackend/src/main/java/com/niit/configuration/DbConfig.java
@@ -0,0 +1,63 @@
+package com.niit.configuration;
+
+
+import java.util.Properties;
+
+
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("com.niit")
+
+public class DbConfig {
+	@Bean(name="dataSource")
+	public DataSource getH2DataSource()
+	{
+	DriverManagerDataSource dataSource=new DriverManagerDataSource();
+	
+	dataSource.setDriverClassName("org.h2.Driver");
+	dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+	dataSource.setUsername("sa");
+	dataSource.setPassword("sa");
+	System.out.println("----DataSource object is created---");
+	return dataSource;
+	}
+	
+	@Bean(name="sessionFactory")
+	public SessionFactory getSessionFactory()
+	{
+		Properties hibernateProp=new Properties();
+		hibernateProp.put("hibernate.hbm2ddl.auto","update");
+		hibernateProp.put("hibernate.dialect","org.hibernate.dialect.H2Dialect");
+		
+		LocalSessionFactoryBuilder factory=new LocalSessionFactoryBuilder(getH2DataSource());
+        factory.addProperties(hibernateProp);
+        
+        factory.addAnnotatedClass();
+
+        
+        System.out.println("---SessionFactory object created----");
+        
+        return factory.buildSessionFactory();
+        		
+	}
+	@Bean(name="txManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory)
+	{
+	System.out.println("----Transaction Object created----");
+	return new HibernateTransactionManager(sessionFactory);
+	}
+
+}
